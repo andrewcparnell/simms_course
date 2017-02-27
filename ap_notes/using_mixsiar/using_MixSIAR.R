80d18e4b18e9069dcf9fbbfea27b0b21bb85c2fb
Andrew Parnell <andrew.parnell@gmail.com>
2017-02-27 22:42:04 +0000

    Updated MixSIAR section

--- ap_notes/using_mixsiar/using_MixSIAR.R
+++
@@ -1,8 +0,0 @@
-# File to load in and play with the MixSIAR GUI
-
-# Set the wd
-setwd("/Volumes/MacintoshHD2/GDrive/Conferences&Talks/SIAR_Glasgow/MixSIAR-2.1.3/R")
-#setwd("~/GDrive/Conferences&Talks/SIAR_Glasgow/MixSIAR-2.1.3/R")
-
-source("mixsiar_gui.r")
-mixsiar_gui()
